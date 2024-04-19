f270c1c3288fd10d06506ac7f8406c47e6cfc681
hassibabch <hassibabch28@gmail.com>
2024-04-19 09:30:04 +0100

    add some animation

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+    let testSlide = document.querySelectorAll('.testItem');
+	let dots = document.querySelectorAll('.dot');
+
+	var counter = 0;
+	function switchTest(currentTest){
+		currentTest.classList.add('active');
+		var testId = currentTest.getAttribute('attr');
+		if(testId > counter){
+			testSlide[counter].style.animation = 'next1 0.5s ease-in forwards';
+			counter = testId;
+			testSlide[counter].style.animation = 'next2 0.5s ease-in forwards';
+		}
+		else if(testId == counter){return;}
+		else{
+			testSlide[counter].style.animation = 'prev1 0.5s ease-in forwards';
+			counter = testId;
+			testSlide[counter].style.animation = 'prev2 0.5s ease-in forwards';
+		}
+		indicators();
+	}
+	function indicators(){
+		for(i = 0; i < dots.length; i++){
+			dots[i].className = dots[i].className.replace(' active', '');
+		}
+		dots[counter].className += ' active';
+	}
