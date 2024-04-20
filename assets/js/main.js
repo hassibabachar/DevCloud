85c4f9b842411d2dc01d115f923a30ddb51f6fab
hassibabachar <hassibabch28@gmail.com>
2024-04-20 15:47:01 +0100

    make navbar responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,44 @@
-hamburger = document.querySelector(".hamburger");
-nav = document.querySelector("nav");
-hamburger.onclick = function() {
-	nav.classList.toggle("active");
-}
-	
-	let testSlide = document.querySelectorAll('.testItem');
-	let dots = document.querySelectorAll('.dot');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".links");
 
-	var counter = 0;
-	function switchTest(currentTest){
-		currentTest.classList.add('active');
-		var testId = currentTest.getAttribute('attr');
-		if(testId > counter){
-			testSlide[counter].style.animation = 'next1 0.5s ease-in forwards';
-			counter = testId;
-			testSlide[counter].style.animation = 'next2 0.5s ease-in forwards';
-		}
-		else if(testId == counter){return;}
-		else{
-			testSlide[counter].style.animation = 'prev1 0.5s ease-in forwards';
-			counter = testId;
-			testSlide[counter].style.animation = 'prev2 0.5s ease-in forwards';
-		}
-		indicators();
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+let testSlide = document.querySelectorAll('.testItem');
+let dots = document.querySelectorAll('.dot');
+
+var counter = 0;
+function switchTest(currentTest){
+	currentTest.classList.add('active');
+	var testId = currentTest.getAttribute('attr');
+	if(testId > counter){
+		testSlide[counter].style.animation = 'next1 0.5s ease-in forwards';
+		counter = testId;
+		testSlide[counter].style.animation = 'next2 0.5s ease-in forwards';
 	}
-	function indicators(){
-		for(i = 0; i < dots.length; i++){
-			dots[i].className = dots[i].className.replace(' active', '');
-		}
-		dots[counter].className += ' active';
+	else if(testId == counter){return;}
+	else{
+		testSlide[counter].style.animation = 'prev1 0.5s ease-in forwards';
+		counter = testId;
+		testSlide[counter].style.animation = 'prev2 0.5s ease-in forwards';
 	}
+	indicators();
+}
+function indicators(){
+	for(i = 0; i < dots.length; i++){
+		dots[i].className = dots[i].className.replace(' active', '');
+	}
+	dots[counter].className += ' active';
+}
